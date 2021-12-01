71969bd3401e02c88f7012bfe335ba60080247e0
Jamal Hasuev <metafra@mail.ru>
2021-12-01 19:39:35 +0300

    add StartScene logic (bg + pulsing text)

--- src/scenes/StartScene.ts
+++ src/scenes/StartScene.ts
@@ -1,10 +1,20 @@
+import Helper from "../classes/Helper"
+
 export default class StartScene extends Phaser.Scene {
+  public helper: any
+  
   constructor() {
     super("StartScene")
+    this.helper = new Helper(this)
   }
 
   create() {
-    // TODO: отобразить фон и пульсирующую надпись "Нажмите чтобы продолжить"
-    // TODO: при нажатии кнопки (клик) - переключить на сцену "MenuScene"
+    this.helper.drawBackground()
+    this.helper.addPulsingText('Нажмите чтобы продолжить')
+    this.input.on("pointerdown", this.onTapDown, this)
+  }
+
+  onTapDown() {
+    this.scene.start('MenuScene')
   }
 }
